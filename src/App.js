0a1295b3cd5dbf66eb2edc695841041d5f73e593
pattyouwehand <pattyouwehand@gmail.com>
2019-10-27 18:50:00 +0100

    header and logo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Logo from './img/inex-logo.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      <div className='app'>
+        <header className='App-header'>
+          <h1 className='App-title'>INEX BREATHWORKS</h1>
+        </header>
+        <div className='main'>
+          <img className='inex-logo' src={Logo} alt='logo'></img>
+         </div>
+         <p>text here</p>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
